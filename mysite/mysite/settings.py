5833dfb68286dbfaaadcb764599fe49bbb41e27c
Alisheva Azalia <azalia.alischeva@yandex.ru>
2021-02-12 23:12:50 +0300

    questions are in admin panel

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UA'
+TIME_ZONE = 'Europe/Simferopol'
 
 USE_I18N = True
 

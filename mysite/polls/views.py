119fd2c0cae79b0ce922ba16b3b4f772dec88200
Alisheva Azalia <azalia.alischeva@yandex.ru>
2021-02-13 07:23:25 +0300

    404 shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
-from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 

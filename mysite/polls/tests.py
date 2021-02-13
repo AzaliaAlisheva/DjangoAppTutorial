6f0c86cf10674d57a1e4690c420e42feb855bbd5
Alisheva Azalia <azalia.alischeva@yandex.ru>
2021-02-13 19:12:25 +0300

    Results view tested (part 5*)

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -10,15 +10,15 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
-class QuestionDetailViewTests(TestCase):
+class QuestionResultsViewTests(TestCase):
     def test_future_question(self):
         future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
+        url = reverse('polls:results', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
+        url = reverse('polls:results', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
